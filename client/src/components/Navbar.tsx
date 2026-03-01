d3b7bccd390c08b4dca466c5f097df81a614e6e0
Rishabh shukla <rishabhshukla2924@gmail.com>
2026-03-01 13:11:04 +0530

    myProjects page

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -52,7 +52,7 @@ const Navbar = () => {
 
       {/* Mobile Menu */}
       {menuOpen && (
-        <div className="fixed inset-0 z-[100] bg-black/60 text-white backdrop-blur flex flex-col items-center justify-center text-lg gap-8 md:hidden transition-transform duration-300">
+        <div className="fixed inset-0 z-100 bg-black/60 text-white backdrop-blur flex flex-col items-center justify-center text-lg gap-8 md:hidden transition-transform duration-300">
           <Link to="/" onClick={() => setMenuOpen(false)}>
             home
           </Link>
