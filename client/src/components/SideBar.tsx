ea40faf80850cee031ab24c283faacbf9ea9b0f2
Rishabh shukla <rishabhshukla2924@gmail.com>
2026-03-07 18:18:38 +0530

    half baked Sidebar for project-development page

--- client/src/components/SideBar.tsx
+++ client/src/components/SideBar.tsx
@@ -1,4 +1,7 @@
-import type { Message, Project } from "../types";
+import { BotIcon, EyeIcon, UserIcon } from "lucide-react";
+import type { Message, Project, Version } from "../types";
+import { Link } from "react-router-dom";
+import { useEffect, useRef } from "react";
 
 interface SideBarProps {
   isMenuOpen: boolean;
@@ -15,13 +18,117 @@ const SideBar = ({
   isGenerating,
   setIsGenerating,
 }: SideBarProps) => {
+  const messageRef = useRef<HTMLDivElement>(null);
+
+  const handleRollBack = (versionId: string) => {};
+
+  useEffect(() => {
+    if (messageRef.current) {
+      messageRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [project.conversation.length, isGenerating]);
+
   return (
     <div
       className={`h-full sm:max-w-sm rounded-xl bg-gray-900 border-gray-800 transition-all ${isMenuOpen ? "max-sm:w-0 overflow-hidden" : "w-full"}`}
     >
       <div className="flex flex-col h-full">
         {/* Message container */}
-        <div className="flex-1 overflow-y-auto no-scrollbar px-3 flex flex-col gap-4"></div>
+        <div className="flex-1 overflow-y-auto no-scrollbar px-3 flex flex-col gap-4">
+          {[...project.conversation, ...project.versions]
+            .sort(
+              (a, b) =>
+                new Date(a.timestamp).getTime() -
+                new Date(b.timestamp).getTime(),
+            )
+            .map((message) => {
+              const isMessage = "content" in message;
+              if (isMessage) {
+                const msg = message as Message;
+                const isUser = msg.role === "user";
+                return (
+                  <div
+                    key={msg.id}
+                    className={`flex items-center gap-3 ${isUser ? "justify-end" : "justify-start"}`}
+                  >
+                    {!isUser && (
+                      <div>
+                        <BotIcon className="size-5 text-white" />
+                      </div>
+                    )}
+                    <div
+                      className={`max-w-[80%] p-2 px-4 rounded-2xl shadow-sm text-sm mt-5 leading-relaxed ${isUser ? "bg-linear-to-r from-indigo-500 to-indigo-600 text-white rounded-tr-none " : "text-gray-100 rounded-tl-none bg-gray-800 "}`}
+                    >
+                      {msg.content}
+                    </div>
+                    {isUser && (
+                      <div>
+                        <UserIcon className="size-5 text-gray-200" />
+                      </div>
+                    )}
+                  </div>
+                );
+              } else {
+                const ver = message as Version;
+                return (
+                  <div
+                    key={ver.id}
+                    className="w-4/5 mx-auto my-2 p-3 rounded-xl bg-gray-800 text-gray-100 shadow flex flex-col gap-2"
+                  >
+                    <div className="text-xs font-medium">
+                      code updated <br />{" "}
+                      <span className="text-gray-500 text-xs font-normal">
+                        {new Date(ver.timestamp).toLocaleString()}
+                      </span>
+                    </div>
+                    <div className="flex items-center justify-between">
+                      {project.current_version_index === ver.id ? (
+                        <button className="px-3 py-1 rounded-md text-xs bg-gray-700">
+                          Current Version
+                        </button>
+                      ) : (
+                        <button
+                          onClick={() => handleRollBack(ver.id)}
+                          className="px-3 py-1 rounded-md text-xs bg-indigo-500 hover:bg-indigo-600 text-white"
+                        >
+                          Roll Back to this version
+                        </button>
+                      )}
+                      <Link
+                        target="_blank"
+                        to={`/preview/${project.id}/${ver.id}`}
+                      >
+                        <EyeIcon className="size-6 p-1 bg-gray-700 hover:bg-indigo-500 transition-colors rounded" />
+                      </Link>
+                    </div>
+                  </div>
+                );
+              }
+            })}
+          {isGenerating && (
+            <div className="flex items-start gap-3 justify-start">
+              <div className="w-8 h-8 rounded-full bg-linear-to-br from-indigo-600 to-indigo-700 flex items-center justify-center">
+                <BotIcon className="size-5 text-white" />
+              </div>
+              {/* 3 dot loader */}
+              <div className="flex gap-1.5 h-full items-end">
+                <span
+                  className="w-2 h-2 bg-gray-600 rounded-full animate-bounce"
+                  style={{ animationDelay: "0s" }}
+                />
+                <span
+                  className="w-2 h-2 bg-gray-600 rounded-full animate-bounce"
+                  style={{ animationDelay: "0.1s" }}
+                />
+                <span
+                  className="w-2 h-2 bg-gray-600 rounded-full animate-bounce"
+                  style={{ animationDelay: "0.2s" }}
+                />
+              </div>
+            </div>
+          )}
+          <div ref={messageRef} />
+        </div>
         {/* Input area */}
         <form action=""></form>
       </div>
