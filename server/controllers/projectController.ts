db9cd1bc30650992f0a6d044e42b11bce5dc41fe
Rishabh shukla <rishabhshukla2924@gmail.com>
2026-05-07 17:49:18 +0530

    projectController Fns to name revision and rollback to prev versions

---
+++ server/controllers/projectController.ts
@@ -0,0 +1,216 @@
+import { Request, Response } from "express";
+import prisma from "../lib/prisma.js";
+import openai from "../config/openai.js";
+
+// Controller Fn to Make Revision (revise our code by adding a new prompt)
+export const makeRevision = async (req: Request, res: Response) => {
+  const userId = req.userId;
+  try {
+    const { projectId } = req.params as { projectId: string };
+    const { message } = req.body;
+
+    const user = await prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+    });
+
+    if (!userId || !user) {
+      return res.status(401).json({ error: "User ID not found in request" });
+    }
+
+    if (user.credits < 5) {
+      return res.status(403).json({ error: "Not enough credits" });
+    }
+
+    if (!message || message.trim() === "") {
+      return res.status(400).json({ error: "Valid prompt is required" });
+    }
+
+    const currentProject = await prisma.websiteProject.findUnique({
+      where: { id: projectId, userId },
+      include: { versions: true },
+    });
+
+    if (!currentProject) {
+      return res.status(404).json({ error: "Project not found" });
+    }
+
+    // updated prompt/message added to the cnversation
+    await prisma.conversation.create({
+      data: {
+        role: "user",
+        content: message,
+        projectId,
+      },
+    });
+
+    //update user credits by decrementing 5
+    await prisma.user.update({
+      where: { id: userId },
+      data: { credits: { decrement: 5 } },
+    });
+
+    //enhance the project by sending the conversation to openai and getting the revised code
+    const promptEnhanceResponse = await openai.chat.completions.create({
+      model: "z-ai/glm-4.5-air:free",
+      messages: [
+        {
+          role: "system",
+          content: `You are a prompt enhancement specialist. The user wants to make changes to their website. Enhance their request to be more specific and actionable for a web developer.
+
+    Enhance this by:
+    1. Being specific about what elements to change
+    2. Mentioning design details (colors, spacing, sizes)
+    3. Clarifying the desired outcome
+    4. Using clear technical terms
+
+Return ONLY the enhanced request, nothing else. Keep it concise (1-2 sentences).`,
+        },
+        {
+          role: "user",
+          content: `User's Request: "${message}"`,
+        },
+      ],
+    });
+
+    const enhancedPrompt = promptEnhanceResponse.choices[0].message.content;
+
+    await prisma.conversation.create({
+      data: {
+        role: "assistant",
+        content: `I've enhanced your prompt to: "${enhancedPrompt}"`,
+        projectId,
+      },
+    });
+
+    await prisma.conversation.create({
+      data: {
+        role: "assistant",
+        content: "Now Making Changes to your website ...",
+        projectId,
+      },
+    });
+
+    //generate website Code
+    const codeGenerationResponse = await openai.chat.completions.create({
+      model: "z-ai/glm-4.5-air:free",
+      messages: [
+        {
+          role: "system",
+          content: `
+                You are an expert web developer. 
+
+    CRITICAL REQUIREMENTS:
+    - Return ONLY the complete updated HTML code with the requested changes.
+    - Use Tailwind CSS for ALL styling (NO custom CSS).
+    - Use Tailwind utility classes for all styling changes.
+    - Include all JavaScript in <script> tags before closing </body>
+    - Make sure it's a complete, standalone HTML document with Tailwind CSS
+    - Return the HTML Code Only, nothing else
+
+    Apply the requested changes while maintaining the Tailwind CSS styling approach.`,
+        },
+        {
+          role: "user",
+          content: `here's the current website code: "${currentProject.current_code}" & The User wants this change: "${enhancedPrompt}"`,
+        },
+      ],
+    });
+
+    const code = codeGenerationResponse.choices[0].message.content || "";
+
+    //update the version
+    const version = await prisma.version.create({
+      data: {
+        code: code
+          .replace(/```[a-z]*\n?/gi, "")
+          .replace(/```$/g, "")
+          .trim(),
+        description: "Changes made",
+        projectId,
+      },
+    });
+
+    await prisma.conversation.create({
+      data: {
+        role: "assistant",
+        content:
+          "I've made the changes to your website. You can now preview it.",
+        projectId,
+      },
+    });
+
+    await prisma.websiteProject.update({
+      where: { id: projectId },
+      data: {
+        current_code: code
+          .replace(/```[a-z]*\n?/gi, "")
+          .replace(/```$/g, "")
+          .trim(),
+        current_version_index: version.id,
+      },
+    });
+
+    res.json({
+      message: "Revision changes made successfully",
+    });
+  } catch (error: any) {
+    await prisma.user.update({
+      where: { id: userId },
+      data: { credits: { increment: 5 } },
+    });
+    console.error(error.code || error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+// Controller Fn to rollback to a previous version
+export const rollbackToVersion = async (req: Request, res: Response) => {
+  try {
+    const userId = req.userId;
+    if (!userId) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
+    const { projectId, versionId } = req.params as {
+      projectId: string;
+      versionId: string;
+    }; //check the versionId type
+
+    const project = await prisma.websiteProject.findUnique({
+      where: { id: projectId, userId },
+      include: { versions: true },
+    });
+    if (!project) {
+      return res.status(404).json({ error: "project not found" });
+    }
+
+    const version = project.versions.find((version) => version.id == versionId);
+    if (!version) {
+      return res.status(404).json({ error: "version not found" });
+    }
+
+    await prisma.websiteProject.update({
+      where: { id: projectId, userId },
+      data: {
+        current_code: version.code,
+        current_version_index: version.id,
+      },
+    });
+
+    await prisma.conversation.create({
+      data: {
+        role: "assistant",
+        content: `You've rolled back to a previous version. You can now preview the website.`,
+        projectId,
+      },
+    });
+
+    res.json({
+      message: "Rollback successful",
+    });
+  } catch (error: any) {
+    console.log(error.code || error.message);
+    res.status(500).json({ message: error.message });
+  }
+};
