cfc4952794f607d34a91eb4d8db0cc4118ee80ba
Rishabh shukla <rishabhshukla2924@gmail.com>
2026-05-11 09:31:24 +0530

    enabled projectRouter

--- server/server.ts
+++ server/server.ts
@@ -4,6 +4,7 @@ import cors from "cors";
 import { toNodeHandler } from "better-auth/node";
 import { auth } from "./lib/auth.js";
 import userRouter from "./routes/userRoutes.js";
+import projectRouter from "./routes/projectRoutes.js";
 
 const app = express();
 
@@ -22,7 +23,9 @@ const port = 3000;
 app.get("/", (req: Request, res: Response) => {
   res.send("Server is Live!");
 });
+
 app.use("/api/user", userRouter);
+app.use("/api/project", projectRouter);
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
